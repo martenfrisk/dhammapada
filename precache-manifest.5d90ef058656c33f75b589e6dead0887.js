8bdc84d42a99fd3abe724ba1fa8382f19c841cde
Marten <marten.frisk@gmail.com>
2020-05-25 08:13:30 +0200

    Updates

---
+++ precache-manifest.5d90ef058656c33f75b589e6dead0887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c812c8c4ef88c1dc7a9e24e2214cc126",
+    "url": "/dhammapada/index.html"
+  },
+  {
+    "revision": "cbfc813eb5bb22ebee8b",
+    "url": "/dhammapada/static/css/main.ee283d9e.chunk.css"
+  },
+  {
+    "revision": "0eb740e7668c9f3ebda5",
+    "url": "/dhammapada/static/js/2.d354caa7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dhammapada/static/js/2.d354caa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfc813eb5bb22ebee8b",
+    "url": "/dhammapada/static/js/main.9b107303.chunk.js"
+  },
+  {
+    "revision": "c27f6fab32da3b800abb",
+    "url": "/dhammapada/static/js/runtime-main.533c0db8.js"
+  },
+  {
+    "revision": "117510b2a7b6e3e237872dd241694367",
+    "url": "/dhammapada/static/media/lotus.117510b2.svg"
+  }
+]);
