1337ac1f0f36a884f462c40a48bac1d464a2ab2f
Marten <marten.frisk@gmail.com>
2020-05-26 13:06:19 +0200

    Fix arrow and scroll navigation

--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { dhp } from '../dhp-1'
 import '../App.css'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, Redirect, useLocation, useHistory, Route } from 'react-router-dom'
 
 export default function Child(props) {
 	let { id } = useParams()
+	let location = useLocation()
 	let currText = dhp[id]
+	let initVersePos = parseInt(currText[1][0])
+	const [arrow, setArrow] = useState()
+	const history = useHistory()
 	let chapterName = currText[0]
 	function addLineBreaks(arr, i) {
 		return arr.slice(1, arr.length).map((item, index) => {
@@ -14,49 +18,31 @@ export default function Child(props) {
 			return <div className={clas}>{item[1]}</div>
 		})
 	}
-	const onWheelTwo = (e) => {
-		if (e.deltaY !== 0) {
-			e.preventDefault()
-			e.stopPropagation()
-			var scrollX = scrollJack(e.deltaX, e.deltaY)
-			document.getElementById('container').scrollBy(scrollX, 0)
-		}
-	}
-	const scrollJack = (dx, dy) => (dy + dx > 0 ? 1 : -1)
 
-	const leftScroll = () => {
-		var container = document.getElementById('container')
-		var containerScrollPosition = document.getElementById('container').scrollLeft
-		container.scrollTo({
-			top: 0,
-			left: containerScrollPosition - 300,
-			behavior: 'smooth'
-		})
-	}
-	const rightScroll = () => {
-		var container = document.getElementById('container')
-		var containerScrollPosition = document.getElementById('container').scrollLeft
-		container.scrollTo({
-			top: 0,
-			left: containerScrollPosition + 300,
-			behavior: 'smooth'
-		})
-	}
+
+
+	// if (key === 'ArrowRight' && currItem < lastItem) {
+	// 	history.push(`./${parseInt(currItem) + 1}`)
+	// 	console.log("Right")
+	//  } else if (key === 'ArrowLeft' && currItem > firstItem) {
+	// 	history.push(`./${parseInt(currItem) - 1}`)
+	// 	console.log("Left")
+	
+	
 
 	useEffect(() => {
-		const handleArrow = (event) => {
-			if (event.key === 'ArrowLeft') {
-				leftScroll()
-			} else if (event.key === 'ArrowRight') {
-				rightScroll()
-			}
-		}
-		window.addEventListener('keydown', handleArrow)
-
+		window.addEventListener('keydown', keyPress);
 		return () => {
-			window.removeEventListener('keydown', handleArrow)
+			window.removeEventListener('keydown', keyPress);
+		};
+	}, []);
+	function keyPress(e) {
+		if(e.key === 'ArrowRight') {
+			setArrow(() => 'ArrowRight')
+		} else if (e.key === 'ArrowLeft') {
+			setArrow(() => 'ArrowLeft')
 		}
-	}, [])
+	}	
 
 	function ScrollResetOnMount() {
 		useEffect(() => {
@@ -69,10 +55,11 @@ export default function Child(props) {
 
 		return null
 	}
-
+	console.log(location.pathname)
 	return (
-		<div className="flex flex-col items-center">
+		<div className="flex flex-col items-center" onKeyDown={keyPress}>
 			<ScrollResetOnMount />
+			{location.pathname === `/${id}` && <Redirect from={`/${id}`} to={`/${id}/${initVersePos}`} />}
 			<div className="pt-12 -mt-1 font-serif text-base text-red-900 cursor-pointer" onClick={props.click}>
 				&#9662;{props.title && `Chapter ${id}: ${chapterName}`}
 			</div>
@@ -82,41 +69,89 @@ export default function Child(props) {
 			>
 				{currText &&
 					currText.slice(1, currText.length).map((item, index) => {
-						return (
-							<div
-								className="flex flex-col items-center justify-start font-serif font-medium leading-9 text-red-900 scroll-child"
-								key={index}
-								onWheel={onWheelTwo}
-							>
-								<div className="pb-2 opacity-75">{item[0]}</div>
+			
+	 	
+						let firstItem = initVersePos
+						let lastItem = parseInt(currText.slice(-1)[0][0])
 
-								{addLineBreaks(item, index)}
-							</div>
+						const onWheelTwo = (e) => {
+							if (e.deltaY !== 0) {
+								e.preventDefault()
+								e.stopPropagation()
+								var scrollX = scrollJack(e.deltaX, e.deltaY)
+								document.getElementById('container').scrollBy(scrollX, 0)
+								if (e.deltaY > 0 && item[0] < lastItem) {
+									history.push(`./${parseInt(item[0]) + 1}`)
+								} else if (e.deltaY < 0 && item[0] > firstItem) {
+									history.push(`./${parseInt(item[0]) - 1}`)
+								}
+							}
+						}
+						const scrollJack = (dx, dy) => (dy + dx > 0 ? 1 : -1)
+
+						
+
+						return (
+							<Route path={`/${id}/${item[0]}`}>
+							{arrow === 'ArrowRight' && item[0] < lastItem ? <Redirect to={`./${parseInt(item[0]) + 1}`} /> : null}
+							{arrow === 'ArrowLeft' && item[0] > firstItem ? <Redirect to={`./${parseInt(item[0]) - 1}`} /> : null}
+
+								<div
+									className="flex flex-col items-center justify-start font-serif font-medium leading-9 text-red-900 scroll-child"
+									key={index}
+									onWheel={onWheelTwo}
+								>
+									<div className="pb-2 opacity-75">{item[0]}</div>
+
+									{addLineBreaks(item, index)}
+									{location.pathname === "/1/1" ?
+									""
+									:
+									(item[0] > firstItem ? (
+										<Link
+											className="absolute top-0 flex items-center justify-start w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base left-4 md:left-20 h-80 mt-72 lg:left-80"
+											to={`./${parseInt(item[0]) - 1}`}
+										>
+											&#10094;
+										</Link>
+									) : (
+										<Link
+											className="absolute top-0 flex items-center justify-start w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base left-4 md:left-20 h-80 mt-72 lg:left-80"
+											to={`/${parseInt(id) - 1}`}
+										>
+											previous
+										</Link>
+									))}
+									{item[0] < lastItem ? (
+										<Link
+											className="absolute top-0 flex items-center justify-end w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base right-4 h-80 mt-72 lg:right-80 md:right-20"
+											to={`./${parseInt(item[0]) + 1}`}
+										>
+											&#10095;
+										</Link>
+									) : (
+										<Link
+											className="absolute top-0 flex items-center justify-end w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base right-4 h-80 mt-72 lg:right-80 md:right-20"
+											to={`/${parseInt(id) + 1}`}
+										>
+											next
+										</Link>
+									)}
+								</div>
+							</Route>
 						)
 					})}
 			</div>
 
-			<div
-				className="absolute top-0 flex items-center justify-start w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base left-4 md:left-20 h-80 mt-72 lg:left-80"
-				onClick={leftScroll}
-			>
-				&#10094;
-			</div>
-			<div
-				className="absolute top-0 flex items-center justify-end w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base right-4 h-80 mt-72 lg:right-80 md:right-20"
-				onClick={rightScroll}
-			>
-				&#10095;
-			</div>
-			<div className="flex opacity-50">
+			<div className="flex font-serif text-sm text-red-900 lowercase opacity-50">
 				{id > 1 && (
-					<Link to={`/${parseInt(id) - 1}`} className="mr-2 font-serif text-sm text-red-900 lowercase">
-						Previous
+					<Link to={`/${parseInt(id) - 1}`} className="mt-px mr-2 text-xs">
+					&#10094;
 					</Link>
-				)}
+				)}&nbsp;chapter&nbsp;
 				{id < 23 && (
-					<Link to={`/${parseInt(id) + 1}`} className="ml-2 font-serif text-sm text-red-900 lowercase">
-						Next
+					<Link to={`/${parseInt(id) + 1}`} className="mt-px ml-2 text-xs">
+					&#10095;
 					</Link>
 				)}
 			</div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import lotus from '../img/lotus.svg'
+import buddha from '../img/buddha-blue.svg'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
+	var hasTouchScreen = false;
+if ("maxTouchPoints" in navigator) { 
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0; 
+} else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+        // Only as a last resort, fall back to user agent sniffing
+        var UA = navigator.userAgent;
+        hasTouchScreen = (
+            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+        );
+    }
+}
+
+
 	return (
 		<div className="flex flex-col items-center self-center mx-auto mt-20 text-2xl">
 			<div className="font-serif text-3xl font-thin font-normal text-red-900">Dhammapada Reader</div>
-			<img src={lotus} className="w-16 h-16 mt-16 mb-16 " alt="Lotus flower" />
-
-			<Link to="/1" className="font-serif text-sm text-red-900 lowercase">
-				Read chapter one
+			<img src={buddha} className="w-24 h-24 mt-16 mb-16 " alt="Lotus flower" />
+			<div className="w-2/3 mb-8 font-serif text-base text-center">
+<p className="mb-2">The Dhammapada is a collection of sayings of the Buddha and one of the most widely read Buddhist scriptures.</p>
+<p className="mb-2">Use this website to read it without distractions.</p>
+<p className="mb-4">Planned features include alternative translations and commentaries.</p>
+			
+			{hasTouchScreen ? <p className="text-gray-800">Swipe sideways or use the side markers to go to next verse.</p> : <p className="text-gray-800">Navigate using your left/right arrow keys, scroll or by clicking the markers on the side of the text.</p>}
+			</div>
+			<Link to="/1" className="font-serif text-sm text-red-900 lowercase border-b border-red-900 border-dashed">
+				start chapter one
 			</Link>
 			<div className="mt-48 text-xs font-light text-red-900 lowercase">sources and copyright information</div>
 		</div>
