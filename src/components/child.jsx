07138ce788b647d02bd1d9021b68993740067c2a
Marten <marten.frisk@gmail.com>
2020-08-02 17:12:27 +0200

    Remove unused deps

--- src/components/child.jsx
+++ src/components/child.jsx
@@ -11,24 +11,25 @@ export default function Child(props) {
 	let location = useLocation()
 	let currText = dhp[id]
 	let initVersePos = parseInt(currText[1][0])
-	const [ arrow, setArrow ] = useState()
+	const [ verse, setVerse ] = useState(initVersePos)
+	const [ chapter, setChapter ] = useState(Number(id))
+	// const [ arrow, setArrow ] = useState({
+	// 	'chapter': id,
+	// 	'verse': initVersePos
+	// })
 	const history = useHistory()
 	let chapterName = currText[0]
 
 	function addLineBreaks(arr, i) {
 		return arr.slice(1, arr.length).map((item, index) => {
-			let ex = 'w-full'
-			let clas = item[0] + ' ' + ex
-			return <div className={clas}>{item[1]}</div>
+			let clas = item[0] + ' w-auto'
+			return (
+				<p key={index} className={clas}>
+					{item[1]}
+				</p>
+			)
 		})
 	}
-
-	// const swipeLeftListener = (item, lastItem) => {
-	// 	item < lastItem && history.push(`./${parseInt(item) + 1}`)
-	// }
-	// const swipeRightListener = (item, firstItem) => {
-	// 	item > firstItem && history.push(`./${parseInt(item) - 1}`)
-	// }
 
 	useEffect(() => {
 		window.addEventListener('keydown', keyPress)
@@ -36,13 +37,41 @@ export default function Child(props) {
 			window.removeEventListener('keydown', keyPress)
 		}
 	}, [])
+
+	const firstItem = initVersePos
+
+	const lastItem = Number(currText.length - 1)
+	console.log(lastItem)
 	function keyPress(e) {
 		if (e.key === 'ArrowRight') {
-			setArrow(() => 'ArrowRight')
+			if (verse === lastItem) {
+				setVerse((prevState) => prevState + 1)
+				setChapter((prevState) => prevState + 1)
+			} else {
+				setVerse((prevState) => prevState + 1)
+			}
+			console.log(`Arrow: right, verse: ${verse}`)
 		} else if (e.key === 'ArrowLeft') {
-			setArrow(() => 'ArrowLeft')
+			let thisVerse = verse
+			if (thisVerse > initVersePos) {
+				thisVerse > 1 && setVerse((prevState) => prevState - 1)
+			} else {
+				thisVerse > 1 && setVerse((prevState) => prevState - 1)
+				chapter > 1 && setChapter((prevState) => prevState - 1)
+			}
+			console.log(`Arrow: left, verse: ${thisVerse}`)
 		}
 	}
+
+	useEffect(
+		() => {
+			let location = {
+				pathname: `/dhammapada/${chapter}/${verse}`
+			}
+			history.push(location)
+		},
+		[ verse, chapter ]
+	)
 
 	function ScrollResetOnMount() {
 		useEffect(() => {
@@ -57,92 +86,84 @@ export default function Child(props) {
 	}
 
 	return (
-		<div className="flex flex-col items-center" onKeyDown={keyPress}>
+		<div className="flex flex-col items-center w-full md:w-2/3" onKeyPressCapture={keyPress}>
 			<ScrollResetOnMount />
 			{location.pathname === `/dhammapada/${id}` && <Redirect from={`./${id}`} to={`./${id}/${initVersePos}`} />}
 			{location.pathname === `/dhammapada/${id}/` && (
 				<Redirect from={`/dhammapada/${id}`} to={`/dhammapada/${id}/${initVersePos}`} />
 			)}
-			<div className="pt-6 -mt-1 font-serif text-base text-red-900 cursor-pointer" onClick={props.click}>
-				&#9662;&nbsp;{props.title 
-				&& (`${id} - ${chapterName}`)}</div>
-				{props.title && (<div className="flex font-serif text-sm text-red-900 lowercase opacity-50 md:bottom-10">
-				{id > 1 && (
-					<Link to={`/dhammapada/${parseInt(id) - 1}`} className="mt-px mr-2 text-xs">
-						&#10094;
-					</Link>
-				)}&nbsp;chapter&nbsp;
-				{id < 23 && (
-					<Link to={`/dhammapada/${parseInt(id) + 1}`} className="mt-px ml-2 text-xs">
-						&#10095;
-					</Link>
-				)}</div>)}
-			
-			<div
-				className="justify-start w-full mt-2 scrolling-touch rounded-lg md:w-1/2 scroll-parent lg:w-1/2"
-				id="container"
-			>
+			<div className="pt-6 mb-4 -mt-1 font-serif text-base text-red-900 cursor-pointer" onClick={props.click}>
+				&#9662;&nbsp;{props.title && `${id} - ${chapterName}`}
+			</div>
+			{props.title && (
+				<div className="flex font-serif text-sm text-red-900 lowercase opacity-50 md:bottom-10">
+					{id > 1 && (
+						<Link to={`/dhammapada/${parseInt(id) - 1}`} className="mt-px mr-2 text-xs">
+							&#10094;
+						</Link>
+					)}&nbsp;chapter&nbsp;
+					{id < 23 && (
+						<Link to={`/dhammapada/${parseInt(id) + 1}`} className="mt-px ml-2 text-xs">
+							&#10095;
+						</Link>
+					)}
+				</div>
+			)}
+
+			<div className="justify-start w-full mt-2 scrolling-touch rounded-lg md:w-1/2 scroll-parent" id="container">
 				{currText &&
 					currText.slice(1, currText.length).map((item, index) => {
-						let firstItem = initVersePos
-						let lastItem = parseInt(currText.slice(-1)[0][0])
-
-					
-
 						return (
 							<Route exact path={`/dhammapada/${id}/${item[0]}`}>
-								{arrow === 'ArrowRight' && item[0] < lastItem ? (
+								{/* {arrow === 'ArrowRight' && item[0] < lastItem ? (
 									<Redirect to={`./${parseInt(item[0]) + 1}`} />
 								) : null}
 								{arrow === 'ArrowLeft' && item[0] > firstItem ? (
 									<Redirect to={`./${parseInt(item[0]) - 1}`} />
-								) : null}
+								) : null} */}
 								<div
-									className="flex flex-col items-center justify-start mx-6 mb-4 scrolling-touch font-serif font-medium leading-9 text-red-900 scroll-child"
+									className="flex flex-wrap items-center justify-center mx-6 mb-4 scrolling-touch font-serif font-medium leading-9 text-red-900 scroll-child"
 									key={index}
 								>
-									<div className="pb-2 opacity-75">{item[0]}</div>
+									<div className="w-full my-6 text-center opacity-75">{item[0]}</div>
+									<div className="w-1/6 text-xs text-left text-red-800 opacity-50 cursor-pointer select-none md:text-base">
+										{location.pathname === '/dhammapada/1/1' ? (
+											''
+										) : item[0] > firstItem ? (
+											<Link className="h-64 pt-24 select-none" to={`./${parseInt(item[0]) - 1}`}>
+												&#10094; {/* < */}
+											</Link>
+										) : (
+											<Link
+												className="h-64 pt-24 select-none"
+												to={`/dhammapada/${parseInt(id) - 1}`}
+											>
+												previous
+											</Link>
+										)}
+									</div>
 
-									{addLineBreaks(item, index)}
-									{location.pathname === '/dhammapada/1/1' ? (
-										''
-									) : item[0] > firstItem ? (
-										<Link
-											className="absolute top-0 flex items-center justify-start w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base left-4 md:left-20 h-80 mt-72 lg:left-80"
-											to={`./${parseInt(item[0]) - 1}`}
-										>
-											&#10094;
-										</Link>
-									) : (
-										<Link
-											className="absolute top-0 flex items-center justify-start w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base left-4 md:left-20 h-80 mt-72 lg:left-80"
-											to={`/dhammapada/${parseInt(id) - 1}`}
-										>
-											previous
-										</Link>
-									)}
-									{item[0] < lastItem ? (
-										<Link
-											className="absolute top-0 flex items-center justify-end w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base right-4 h-80 mt-72 lg:right-80 md:right-20"
-											to={`./${parseInt(item[0]) + 1}`}
-										>
-											&#10095;
-										</Link>
-									) : (
-										<Link
-											className="absolute top-0 flex items-center justify-end w-12 text-xs text-red-800 opacity-50 cursor-pointer select-none md:text-base right-4 h-80 mt-72 lg:right-80 md:right-20"
-											to={`/dhammapada/${parseInt(id) + 1}`}
-										>
-											next
-										</Link>
-									)}
+									<div className="w-4/6 text-left ">{addLineBreaks(item, index)}</div>
+
+									<div className="flex items-center justify-end w-1/6 text-xs text-right text-red-800 opacity-50 cursor-pointer select-none md:text-base">
+										{item[0] < lastItem ? (
+											<Link className="h-64 pt-24 select-none" to={`./${parseInt(item[0]) + 1}`}>
+												&#10095; {/* > */}
+											</Link>
+										) : (
+											<Link
+												className="h-64 pt-24 select-none"
+												to={`/dhammapada/${parseInt(id) + 1}`}
+											>
+												next
+											</Link>
+										)}
+									</div>
 								</div>
 							</Route>
 						)
 					})}
 			</div>
-
-			
 		</div>
 	)
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,40 +3,50 @@ import buddha from '../img/buddha-blue.svg'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
-	var hasTouchScreen = false;
-if ("maxTouchPoints" in navigator) { 
-    hasTouchScreen = navigator.maxTouchPoints > 0;
-} else if ("msMaxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.msMaxTouchPoints > 0; 
-} else {
-    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-    if (mQ && mQ.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-    } else if ('orientation' in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-    } else {
-        // Only as a last resort, fall back to user agent sniffing
-        var UA = navigator.userAgent;
-        hasTouchScreen = (
-            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-    }
-}
-
+	var hasTouchScreen = false
+	if ('maxTouchPoints' in navigator) {
+		hasTouchScreen = navigator.maxTouchPoints > 0
+	} else if ('msMaxTouchPoints' in navigator) {
+		hasTouchScreen = navigator.msMaxTouchPoints > 0
+	} else {
+		var mQ = window.matchMedia && matchMedia('(pointer:coarse)')
+		if (mQ && mQ.media === '(pointer:coarse)') {
+			hasTouchScreen = !!mQ.matches
+		} else if ('orientation' in window) {
+			hasTouchScreen = true // deprecated, but good fallback
+		} else {
+			// Only as a last resort, fall back to user agent sniffing
+			var UA = navigator.userAgent
+			hasTouchScreen =
+				/\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+		}
+	}
 
 	return (
 		<div className="flex flex-col items-center self-center mx-auto mt-6 text-2xl">
-			<div className="font-serif text-3xl font-thin font-normal text-red-900">Dhammapada Reader</div>
+			<div className="font-serif text-3xl font-thin text-red-900">Dhammapada Reader</div>
 			<img src={buddha} className="w-24 h-24 mt-10 mb-4 " alt="Lotus flower" />
 			<div className="w-2/3 mb-8 font-serif text-base text-center">
-<p className="mb-2">The Dhammapada is a collection of sayings of the Buddha and one of the most widely read Buddhist scriptures.</p>
-<p className="mb-2">Use this website to read it without distractions.</p>
-<p className="mb-4">Planned features include alternative translations and commentaries.</p>
-			
-			{hasTouchScreen ? <p className="text-gray-800">Swipe sideways or use the side markers to go to next verse.</p> : <p className="text-gray-800">Navigate using your left/right arrow keys, scroll or by clicking the markers on the side of the text.</p>}
+				<p className="mb-2">
+					The Dhammapada is a collection of sayings of the Buddha and one of the most widely read Buddhist
+					scriptures.
+				</p>
+				<p className="mb-2">Use this website to read it without distractions.</p>
+				<p className="mb-4">Planned features include alternative translations and commentaries.</p>
+
+				{hasTouchScreen ? (
+					<p className="text-gray-800">Swipe sideways or use the side markers to go to next verse.</p>
+				) : (
+					<p className="text-gray-800">
+						Navigate using your left/right arrow keys, scroll or by clicking the markers on the side of the
+						text.
+					</p>
+				)}
 			</div>
-			<Link to="/dhammapada/1" className="font-serif text-sm text-red-900 lowercase border-b border-red-900 border-dashed">
+			<Link
+				to="/dhammapada/1"
+				className="font-serif text-sm text-red-900 lowercase border-b border-red-900 border-dashed"
+			>
 				start chapter one
 			</Link>
 			<div className="mt-48 text-xs font-light text-red-900 lowercase">sources and copyright information</div>
